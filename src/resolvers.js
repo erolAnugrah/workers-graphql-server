1cacb21f4a35ac88d11c0288bb649e019133d302
chaeroldi.anugrah <erol_its@yahoo.com>
2023-01-26 09:26:33 +0700

    fix: try again

--- src/resolvers.js
+++ src/resolvers.js
@@ -3,6 +3,8 @@ module.exports = {
     pokemon: async (_source, { id }, { dataSources }) => {
       return dataSources.pokemonAPI.getPokemon(id)
     },
-    version: () => "1.0.21"
+    version: async (_source) => {
+      return "1.0.21"
+    }
   },
 }
