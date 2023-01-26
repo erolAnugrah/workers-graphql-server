9b7edad11aae38f9471c7a1a0b224859d59a4a1f
chaeroldi.anugrah <erol_its@yahoo.com>
2023-01-26 13:01:23 +0700

    fix: add versin

--- src/resolvers.js
+++ src/resolvers.js
@@ -4,6 +4,9 @@ module.exports = {
 
       return dataSources.pokemonAPI.getPokemon(id)
     },
+    version: async () => {
+      return "1.0.02"
+    }
     // clusterCategory: async (_source, { param }, { dataSources }) => {
     //   console.log(param)
     //   return dataSources.clusterCategory.getClusterCategories(param)
